1cbb151da23a7ac214e66df00bde8cdbbc840901
pandada8 <pandada8@gmail.com>
2015-03-18 01:50:25 +0800

    Add backend again

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import model
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add_to_list', methods=["POST"])
+def AddToQueue():
+    # save the data to the database
+    if set(request.form) >= set(['username', 'password', 'mail']):
+        with model.db.transaction():
+            model.Task.create(
+                create_time=datetime.now(),
+                username=request.form.get('username'),
+                password=request.form.get('password'),
+                mail=request.form.get('mail'),
+            )
+            return render_template('waiting.html')
+    return json.dumps({"msg": "Finished"})
+
+app.secret_key = os.urandom(12)
+
+
+if __name__ == "__main__":
+    app.secret_key = "123456"  # easy for debuging
+    app.debug = True
+    app.run()
---
+++ model.py
@@ -0,0 +1,33 @@
+from peewee import Model, CharField, DateTimeField, TextField, IntegerField, CharField
+from playhouse import db_url
+from datetime import datetime
+
+db = db_url.connect("sqlite:///default.db")
+
+
+class BaseModel(Model):
+
+    @classmethod
+    def get_default(cls, **kwargs):
+        try:
+            return cls.get(**kwargs)
+        except cls.DoesNotExit:
+            pass
+
+    def to_dict(self):
+        return self._data
+
+    class Meta:
+        database = db
+
+class Task(Model):
+
+    create_time = DateTimeField()
+    finish_time = DateTimeField(null=True)
+    username = CharField()
+    password = CharField()
+    mail = CharField()
+    parse_result = TextField(null=True)
+    status = IntegerField(default=0)  # 0-queued 1-finished 2-failed
+
+db.create_tables([Task], safe=True)
